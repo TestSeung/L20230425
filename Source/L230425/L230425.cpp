b71503f73f7d4e299a62f35ca9098f4233992a45
TestSeung <sjlim589@hanmail.net>
2023-04-25 09:54:17 +0900

    First Commit

---
+++ Source/L230425/L230425.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L230425.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L230425, "L230425" );
